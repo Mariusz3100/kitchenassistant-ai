389cba751b4451acfc7e989a1f98d02f264f55f9
M <armaniel667@gmail.com>
2019-08-22 10:46:15 +0200

    Start of the project. Simple Spring boot template.

---
+++ NlpAi/src/main/java/mariusz/ambroziak/kassistant/ai/NlpAiApplication.java
@@ -0,0 +1,13 @@
+package mariusz.ambroziak.kassistant.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlpAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlpAiApplication.class, args);
+	}
+
+}
---
+++ NlpAi/src/test/java/mariusz/ambroziak/kassistant/ai/NlpAiApplicationTests.java
@@ -0,0 +1,13 @@
+package mariusz.ambroziak.kassistant.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlpAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
